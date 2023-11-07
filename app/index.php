0f20661d43bbe554ea6d2a186476d461d39de90e
Gabriel Mejia <20220192@itla.edu.do>
2023-11-06 20:36:04 -0400

    init

---
+++ app/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola Mundo PHP</title>
+</head>
+<body>
+    <h1>Hola Mundo PHP de parte de Gabriel Mejia</h1>
+
+    <?php
+      echo "<p>Conexión a la base de datos exitosa.</p>";
+    // Conexión a la base de datos
+    $mysqli = new mysqli("mariadb", "miapp", "mi@pp", "miapp");
+
+    // Verificar la conexión
+    if ($mysqli->connect_error) {
+        die("Error de conexión a la base de datos: " . $mysqli->connect_error);
+    }
+
+    echo "<p>Conexión a la base de datos exitosa.</p>";
+
+    // Realizar una consulta
+    $result = $mysqli->query("SELECT '¡Hola Mundo desde la base de datos!' AS message");
+
+    if ($result) {
+        $row = $result->fetch_assoc();
+        echo "<p>Respuesta de la base de datos: " . $row["message"] . "</p>";
+        $result->close();
+    } else {
+        echo "Error en la consulta: " . $mysqli->error;
+    }
+
+    // Cerrar la conexión
+    $mysqli->close();
+    ?>
+
+</body>
+</html>
